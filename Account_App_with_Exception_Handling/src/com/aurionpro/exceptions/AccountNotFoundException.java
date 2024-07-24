ed5544ae45e943c48344a7f27f7c38a8f8d2a5f2
Devan <devankhandagale@gmail.com>
2024-07-24 20:39:43 +0530

    Delete Account_App_with_Exception_Handling directory

--- Account_App_with_Exception_Handling/src/com/aurionpro/exceptions/AccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.aurionpro.exceptions;
-
-public class AccountNotFoundException extends RuntimeException {
-	public String toMessage() {
-		return "Account not Found";
-	}
-}
--- Account_App_with_Exception_Handling/src/com/aurionpro/exceptions/InvalidAmountException.java
+++
@@ -1,7 +0,0 @@
-package com.aurionpro.exceptions;
-
-public class InvalidAmountException extends RuntimeException {
-	public String toMessage() {
-		return "Enter a valid Amount";
-	}
-}
